83676dc75c15331888acb83d9e7c65283600bcb9
Mark Milkis <mark.milkis@parsons.com>
2025-07-22 17:53:15 -0400

    fix: update version to 0.1.1.dev3 in _version.py and add it to .gitignore

--- src/report_compiler/_version.py
+++ src/report_compiler/_version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.1.dev2+g9ae3d0a.d20250722'
-__version_tuple__ = version_tuple = (0, 1, 1, 'dev2', 'g9ae3d0a.d20250722')
+__version__ = version = '0.1.1.dev3+g204a9df.d20250722'
+__version_tuple__ = version_tuple = (0, 1, 1, 'dev3', 'g204a9df.d20250722')
